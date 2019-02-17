a382f2bc6e9f89c751bf4f32f02f2c35f2fe8ea7
ｅｌｅｍ＇ <47723635+doElem@users.noreply.github.com>
2019-02-17 22:22:34 +0000

    Add files via upload

---
+++ amazon/esx_amazon/locales/pt.lua
@@ -0,0 +1,15 @@
+Locales['pt'] = {
+  -- Shop related
+  ['delivery_shop_open'] = 'Pressiona ~INPUT_CONTEXT~ para acessares a ~y~Amazon~s~.',
+  ['delivery_shop_item'] = '$%s',
+  ['delivery_shop_title'] = 'Bem-vindo à Amazon! Pede as encomendas rápido!',
+  ['delivery_shop_cannot_afford'] = 'Não tens condições para pagar esta entrega!',
+
+  -- Global message related
+  ['global_title'] = 'Amazon',
+  ['global_subject'] = 'Encomenda a caminho!',
+  ['global_msg'] = '~y~%s~s~ acabou de pedir uma encomenda em ~o~%s~s~, vai tentar roubá-la!',
+
+  -- Blip related
+  ['blip_shop'] = 'Loja Amazon',
+}
